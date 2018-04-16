0e969e0fa5a8bfb1eecc721f60fcc3cbbee59af9
Dmitry Ovsyanko <do_@pochta.ru>
2018-04-16 16:20:16 +0300

    task_notes search

--- back/lib/Content/task_notes.pm
+++ back/lib/Content/task_notes.pm
@@ -12,12 +12,36 @@ sub select_task_notes {
 
 	$_REQUEST {sort} = [{field => "id", direction => "asc"}];
 	
+	my $note = undef;
+	
 	if ($_REQUEST {searchLogic} eq 'OR') {
 		
 		$note = $_REQUEST {search} [0] {value};
 		
 		$_REQUEST {search} = [];
 	
+	}
+	else {
+	
+		my @r = ();
+		
+		foreach my $s (@{$_REQUEST {search}}) {
+		
+			if ($s -> {field} eq 'note') {
+
+				$note = $s -> {value};
+
+			}
+			else {
+
+				push @r, $s;
+
+			}
+		
+		}
+
+		$_REQUEST {search} = \@r;
+
 	}
 	
 	my $filter = w2ui_filter ();
@@ -29,6 +53,7 @@ sub select_task_notes {
 	}
 
 	sql ({}, task_notes => $filter,
+		'tasks(uuid)'
 	);
 
 }
--- front/root/_/app/js/view/task_notes.js
+++ front/root/_/app/js/view/task_notes.js
@@ -2,7 +2,7 @@ define ([], function () {
     
     return function (data, view) {
     
-        $('title').text ('Дела')
+        $('title').text ('Всеобщая переписка')
 
         $('main').w2regrid ({ 
         
@@ -14,16 +14,27 @@ define ([], function () {
                 toolbarAdd: true,
             },
 
+            searches: [
+                {field: 'note',    caption: 'Содержимое', type: 'text'},
+                {field: 'id_user_from', caption: 'Автор', type: 'enum', options: {items: data.users.items}},
+                {field: 'id_user_to', caption: 'Адресат', type: 'enum', options: {items: data.users.items}},
+                {field: 'ts',    caption: 'Дата', type: 'date'},
+            ],
+
             columns: [
 
-                {field: 'ts',                caption: 'Дата',      size: 40, render: function (i) {return i.ts.substring (0,19)}},
-                {field: 'label',             caption: 'Тема',      size: 100},
+                {field: 'ts',                caption: 'Дата',      size: 20, render: function (i) {return i.ts.substring (0,19)}},
+                {field: 'label',             caption: 'Заголовок',      size: 100},
                 {field: 'id_user_from', caption: 'Автор',          size: 30, render: function (i) {return data.users [i.id_user_from]}},
                 {field: 'id_user_to', caption: 'Адресат',          size: 30, render: function (i) {return data.users [i.id_user_to]}},
 
             ],
                         
             url: '_back/?type=task_notes',
+            
+            onDblClick: function (e) {
+                openTab ('/tasks/' + this.get (e.recid) ['task'] ['uuid'])
+            }
             
         }).refresh ();
         
