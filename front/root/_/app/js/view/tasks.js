cae5aabd7f8e629f66560595fa47ec8488237a23
Dmitry Ovsyanko <dovsyanko@cheby.ru>
2018-09-07 10:39:49 +0300

    tasks roster cosmetics

--- front/root/_/app/js/view/tasks.js
+++ front/root/_/app/js/view/tasks.js
@@ -16,17 +16,14 @@ define ([], function () {
             
             toolbar: {
                 items: [
-                    {type: 'button', id: 'b1', caption: 'Что у меня в работе', onClick: $_DO.show_required_tasks},
-                    {type: 'button', id: 'b2', caption: 'Что я просил', onClick: $_DO.show_created_tasks},
+                    {type: 'button', id: 'b1', caption: 'Что ожидается от меня', onClick: $_DO.show_required_tasks},
+                    {type: 'button', id: 'b2', caption: 'Чего я ожидаю', onClick: $_DO.show_created_tasks},
                 ],
-//                onClick: function (target, data) {
-//                    console.log(target);
-//                }
             },
             
             columnGroups : [
                 {span: 4, caption: 'Дело'},
-                {span: 3, caption: 'Последняя реплика'},
+                {span: 5, caption: 'Последнее сообщение'},
             ],
             
             searches: [
@@ -45,9 +42,11 @@ define ([], function () {
                 {field: 'label',             caption: 'Тема',      size: 100},
                 {field: 'id_user',           caption: 'На ком сейчас',    size: 40, hidden: true, render: function (i) {return !i.id_user ? '' : data.users [i.id_user]}},
 
-                {field: 'task_note.ts',      caption: 'Дата',      size: 40, render: function (i) {return i.task_note.ts.substring (0,19)}},
-                {field: 'task_note.label',   caption: 'Заголовок', size: 100},
-                {field: 'task_note.id_user_from', caption: 'Автор',   size: 30, render: function (i) {return data.users [i.task_note.id_user_from]}},
+                {field: 'task_note.ts',           caption: 'Дата',    size: 40, render: function (i) {return i.task_note.ts.substring (0,19)}},
+                {field: 'task_note.label',        caption: 'Текст',   size: 100},
+                {field: 'task_note.id_user_from', caption: 'Автор',   size: 30, voc: data.users},
+                {field: '_',                      caption: ' ',       size: 5, render: function () {return '&rarr;'}},
+                {field: 'task_note.id_user_to',   caption: 'Адресат', size: 30, voc: data.users},
 
             ],
                         
