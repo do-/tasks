bd8db2a52ac536631abbd8fc44e646448c5384e1
Dmitry Ovsyanko <do_@pochta.ru>
2018-12-30 20:51:55 +0300

    js decomposition

--- back/lib/Config.js
+++ back/lib/Config.js
@@ -1,29 +1,4 @@
-const Dia  = require ('./Dia.js')
-
-const url  = require ('url')
-
-function parse_request (rq) {
-
-    var uri = url.parse (rq.url)
-
-    var params = new URLSearchParams (uri.search);
-
-    $_REQUEST = {}; for (var k of ['type', 'id', 'action', 'part']) if (params.has (k)) $_REQUEST [k] = params.get (k)
-
-}
-
-function out_json (rp, code, page) {
-    rp.statusCode = code
-    rp.setHeader ('Content-Type', 'application/json')
-    rp.end (JSON.stringify (page))
-}
-
-function out_error (rp, ex) {
-    function s4 () {return Math.floor((1 + Math.random()) * 0x10000).toString (16).substring (1)}
-    var id = s4 () + s4 () + '-' + s4 () + '-' + s4 () + '-' + s4 () + '-' + s4 () + s4 () + s4 ()
-    darn ([id, ex])
-    out_json (rp, 500, {success: false, id: id, dt: new Date ().toJSON ()})
-}
+const Dia = require ('./Ext/Dia/Dia.js')
 
 function handle_valid_request (rp) {
 
@@ -40,19 +15,12 @@ function handle_valid_request (rp) {
     if (!fun) return out_error (rp, `No ${name} defined for ${$_REQUEST.type}`)
 
     new Promise (fun)
-        .then  ((it) => out_json  (rp, 200, {success: true, content: it}))
-        .catch ((ex) => out_error (rp, ex))
+        .then  ((it) => Dia.out_json  (rp, 200, {success: true, content: it}))
+        .catch ((ex) => Dia.out_error (rp, ex))
 
 }
 
-function handler (rq, rp) {
-
-    parse_request (rq)  
-  
-darn ($_REQUEST)
-  
+Dia.listen ((rq, rp) => {
+    Dia.parse_request (rq)    
     handle_valid_request (rp)
-
-}
-
-Dia.listen (handler)
+})
