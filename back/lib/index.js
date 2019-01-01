2a2d6f23a326e74635657532a01fefe676b9596c
Dmitry Ovsyanko <do_@pochta.ru>
2019-01-01 19:38:15 +0300

    get_method_name

--- back/lib/index.js
+++ back/lib/index.js
@@ -1,37 +1,31 @@
 const Dia = require ('./Ext/Dia/Dia.js')
 
-function get_function_name (q) {
-    if (q.part)   return 'get_' + q.part
-    if (q.action) return 'do_'  + q.action
-    return q.id ? 'get': 'select'
-}
-
 class WebUiRequest extends Dia.Request {
+    
+    get_method_name () {
+        let q = this.q
+        if (q.part)   return 'get_' + q.part
+        if (q.action) return 'do_'  + q.action
+        return q.id ? 'get': 'select'
+    }
 
     async run () {
 
-        let function_name = get_function_name (this.q)
-
-        let label = `${this.q.type} ${function_name} ${this.uuid}`
+        let label = `${this.module_name} ${this.method_name} ${this.uuid}`
         console.time (label)
 
-        var module = Dia.require_fresh (this.q.type)
+        var module = Dia.require_fresh (this.module_name)
 
         try {
 
             if (!module) throw `No code defined for ${this.q.type}`
 
-            var fun = module [function_name]; if (!fun) throw `No ${name} defined for ${this.q.type}`
+            var fun = module [this.method_name]; if (!fun) throw `No ${name} defined for ${this.module_name}`
 
-            this.out_json (200, {success: true, content: await fun.call (this)})
-
-        }
-        catch (x) {
-
-            darn ([this.uuid, x])
-            this.out_json (500, {success: false, id: this.uuid, dt: new Date ().toJSON ()})
+            this.out (await fun.call (this))
 
         }
+        catch (x) {this.carp (x)}
 
         console.timeEnd (label)
 
