760f9c6674fff90a1a5bca3baaf39d73622d7e92
Dmitry Ovsyanko <do_@pochta.ru>
2019-01-02 18:44:27 +0300

    fetching data from db

--- back/lib/Content/tasks.js
+++ back/lib/Content/tasks.js
@@ -4,9 +4,13 @@ const _ = exports
 
 _.select = async function ($_REQUEST) {
 
-//darn (['select this', this])
+    let params = ['1']
 
-    return {"portion":100,"tasks":[{"id_user":1,"id_last_task_note":"60","task_note":{"is_illustrated":0,"id_task":"22","body":"","label":"nbcmnc","fake":0,"id":"60","ts":"2018-04-16 18:22:20.599025","uuid":"73f7ba7d-8c4a-447b-a26d-9cb42a424dd2","id_user_from":1,"id_user_to":1,"ext":null},"id":"22","fake":0,"label":"E-mail","ts":"2018-04-16 15:46:46.99786","uuid":"849059f5-dcd1-45ba-90fa-72d131e4769d"}],"cnt":"1"}
+    return {
+        portion:100,
+        tasks: await this.db.select_all ('SELECT tasks.label AS "label" , tasks.id_user AS "id_user" , tasks.ts AS "ts" , tasks.id_last_task_note AS "id_last_task_note" , tasks.id AS "id" , tasks.fake AS "fake" , tasks.uuid AS "uuid" , task_notes.uuid AS "task_note.uuid" , task_notes.id_user_from AS "task_note.id_user_from" , task_notes.ext AS "task_note.ext" , task_notes.id_user_to AS "task_note.id_user_to" , task_notes.id_task AS "task_note.id_task" , task_notes.is_illustrated AS "task_note.is_illustrated" , task_notes.label AS "task_note.label" , task_notes.body AS "task_note.body" , task_notes.fake AS "task_note.fake" , task_notes.id AS "task_note.id" , task_notes.ts AS "task_note.ts" FROM tasks LEFT JOIN task_notes ON (id_last_task_note = task_notes.id ) WHERE 1=1 AND (tasks.id_user IN ($1)) AND tasks.fake = 0 ORDER BY tasks.id ASC LIMIT 100 OFFSET 0', params),
+        cnt: await this.db.select_scalar ('SELECT COUNT(*) FROM tasks LEFT JOIN task_notes ON (id_last_task_note = task_notes.id ) WHERE 1=1 AND (tasks.id_user IN ($1)) AND tasks.fake = 0', params),
+    }
 
 }
 
@@ -14,10 +18,6 @@ _.select = async function ($_REQUEST) {
 
 _.get_vocs = async function ($_REQUEST) {
 
-//darn (['get_vocs this', this])
-
-//    return await $_DB.select_all ("SELECT id, label, fake FROM users WHERE 1=1 AND fake = 0 AND id > 0 ORDER BY 2");
-
-    return {"users":[{"label":"Овсянко Дмитрий","id":1,"fake":0},{"label":"Потапов Виталий","fake":0,"id":2}]}
+    return {users: await this.db.select_all ("SELECT id, label, fake FROM users WHERE 1=1 AND fake = 0 AND id > 0 ORDER BY 2")}
 
 }
