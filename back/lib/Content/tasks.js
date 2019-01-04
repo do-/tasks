1ce1a94f572d1e315f1a57bec2e04454818c0ccd
Dmitry Ovsyanko <do_@pochta.ru>
2019-01-04 20:15:17 +0300

    add_all_cnt

--- back/lib/Content/tasks.js
+++ back/lib/Content/tasks.js
@@ -5,23 +5,18 @@ module.exports = {
 // #############################################################################
 
 select: async function () {
-darn (this.db)
-    let q = new Dia.DB.Query (this.db.model,
+    
+    let data = {portion: 100}
+    
+    return await this.db.add_all_cnt (data, [
+
         {tasks : {
             id_user: 1
         }}, 
+        
         'task_notes(*) ON id_last_task_note',
-    )
 
-    let [all, cnt] = await this.db.select_all_cnt (q.sql, q.params, 100)
-
-//    let [all, cnt] = await this.db.select_all_cnt ('SELECT tasks.label AS "label" , tasks.id_user AS "id_user" , tasks.ts AS "ts" , tasks.id_last_task_note AS "id_last_task_note" , tasks.id AS "id" , tasks.fake AS "fake" , tasks.uuid AS "uuid" , task_notes.uuid AS "task_note.uuid" , task_notes.id_user_from AS "task_note.id_user_from" , task_notes.ext AS "task_note.ext" , task_notes.id_user_to AS "task_note.id_user_to" , task_notes.id_task AS "task_note.id_task" , task_notes.is_illustrated AS "task_note.is_illustrated" , task_notes.label AS "task_note.label" , task_notes.body AS "task_note.body" , task_notes.fake AS "task_note.fake" , task_notes.id AS "task_note.id" , task_notes.ts AS "task_note.ts" FROM tasks LEFT JOIN task_notes ON (id_last_task_note = task_notes.id ) WHERE 1=1 AND (tasks.id_user IN (?)) AND tasks.fake = 0 ORDER BY tasks.id', [1], 100)
-    
-    return {
-        portion:100,
-        tasks: all,
-        cnt: cnt,
-    }
+    ], data.portion)
 
 },
 
