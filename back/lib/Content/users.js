bd0111640a21b0f7ad616b3b73c77a85571de0f9
Dmitry Ovsyanko <do_@pochta.ru>
2019-01-05 11:27:29 +0300

    users get

--- back/lib/Content/users.js
+++ back/lib/Content/users.js
@@ -2,14 +2,28 @@ const Dia = require ('../Ext/Dia/Dia.js')
 
 module.exports = {
 
-// #############################################################################
+////////////
+  select: //
+////////////
 
-select: function () {
+    function () {
+
+        return this.db.add ({}, [{users: {'id >': 0}}, 'roles AS role'])
+
+    },
+
+/////////
+  get: //
+/////////
+
+    function () {
         
-    return this.db.add ({}, [{users: {'id >': 0}}, 'roles AS role'])
+        return this.db.get ([{users: 
 
-},
+            {uuid: this.q.id},
 
-// #############################################################################
+        }, 'roles AS role'])
+
+    },
 
 }
