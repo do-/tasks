9f47738aea33044ae8effe67153c2995844a0008
Dmitry Ovsyanko <do_@pochta.ru>
2019-01-12 18:14:26 +0300

    using http.createServer directly

--- back/lib/index.js
+++ back/lib/index.js
@@ -1,5 +1,6 @@
-const fs  = require ('fs')
-const Dia = require ('./Ext/Dia/Dia.js')
+const fs   = require ('fs')
+const http = require ('http')
+const Dia  = require ('./Ext/Dia/Dia.js')
 
 const Content = require ('./Content.js')
 const model   = new (require ('./Model.js')) ({path: './Model'})
@@ -8,12 +9,18 @@ const db_pool = Dia.DB.Pool ($_CONF.db, model)
 if (!$_CONF.pics) throw '$_CONF.pics is not defined'
 if (!fs.statSync ($_CONF.pics).isDirectory ()) throw $_CONF.pics + 'is not a direcory'
 
-Dia.HTTP.listen ((rq, rp) => {
+http.createServer (
 
-    new Content ({
-        db_pools     : {db: db_pool},
-        http_request : rq, 
-        http_response: rp
-    }).run ()
+    (rq, rp) => {
 
+        new Content ({
+            db_pools     : {db: db_pool},
+            http_request : rq, 
+            http_response: rp
+        }).run ()
+
+    }
+
+).listen ($_CONF.listen, function () {
+    darn ('tasks app is listening to HTTP at ' + this._connectionKey)
 })
