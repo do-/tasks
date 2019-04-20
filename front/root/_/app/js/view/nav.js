eaf4400ad1e128ccfbcc85d543b8b61fa246dced
Dmitry Ovsyanko <do_@pochta.ru>
2019-04-20 08:28:57 +0300

    nav no fill

--- front/root/_/app/js/view/nav.js
+++ front/root/_/app/js/view/nav.js
@@ -2,36 +2,44 @@ define ([], function () {
     
     return function (data, view) {
 
+        if (!$_USER) return
+
         var $nav = $('nav')
-           
-        if ($_USER) {
         
-            fill (view, data, $nav.show ())
+        $nav.html (`
+            <header>
+                <button name=open_tasks title="Дела" class="svg-menu"></button>
+                <button name=open_task_notes title="Всеобщая переписка" class="svg-xchg"></button>
+                <button name=open_help title="Справка" class="svg-help"></button>
+                <button name=open_users title="Пользователи" class="svg-users"></button>
+            </header>
+
+            <footer>
+                <button name=open_users_select title="Корреспонденты" class="svg-users"></button>
+                <button name=open_user_password title="Смена пароля" class="svg-keys"></button>
+                <button name=open_settings title="Настройки" class="svg-settings"></button>
+                <button name=logout title="Выход" class="logout svg-logout" data-question="Завершить работу в системе?"></button>
+            </footer>
+        `)
+        
+        $('button', $nav).each (function () {
+        
+            let $this = $(this)
             
-            $('body > nav header button').after ('<hr>')
-            $('body > nav footer button').before ('<hr>')
+            let name = this.name
             
-            $('body > nav header button').each (function () {
+            if (!data._can [name]) return $this.remove ()
             
-                var $this = $(this)
-                
-                if (location.href.indexOf ($this.attr ('name').substr (5)) < 0) return
-                
-                $this.addClass ('active')
-                
-                return false
-            
-            })
-            
-/*            
-            var color = data.stand.color
-            
-            if (color) {            
-                $('nav').css ({background: color})
-                $('nav button').css ({'border-color': color})            
-            }
-*/        
-        }
+            clickOn ($this, $_DO [name + '_nav'])
+
+            if (location.href.indexOf (name.substr (5)) >= 0) $this.addClass ('active')
+        
+        })
+        
+        $('body > nav header button').after ('<hr>')
+        $('body > nav footer button').before ('<hr>')
+        
+        $nav.show ()
 
     }
 
