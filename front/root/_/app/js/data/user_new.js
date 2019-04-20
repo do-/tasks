af5e0cbdcc0f19c77ddc5c67d4e7a303b5bd1853
Dmitry Ovsyanko <do_@pochta.ru>
2019-04-20 16:52:44 +0300

    user_new: fill -> use.jq

---
+++ front/root/_/app/js/data/user_new.js
@@ -0,0 +1,31 @@
+$_DO.update_user_new = function (e) {
+
+    var f = w2ui ['users_new_form']
+
+    var v = f.values ()
+    
+    if (!v.id_role) die ('id_role', 'Укажите, пожалуйста, роль')
+    if (!v.label)   die ('label', 'Укажите, пожалуйста, ФИО пользователя')
+    if (!v.login)   die ('login', 'Укажите, пожалуйста, login пользователя')
+    
+    f.lock ()
+
+    query ({action: 'create'}, {data: v}, function (data) {
+        
+        w2popup.close ()
+        
+        var grid = w2ui ['usersGrid']
+        
+        grid.reload (grid.refresh)
+
+        w2confirm ('Пользователь зарегистрирован. Открыть его карточку?').yes (function () {openTab ('/users/' + data.uuid)})
+    
+    })
+
+}
+
+$_GET.user_new = async function (o) {
+
+    return $('body').data ('data')
+    
+}
--- front/root/_/app/js/data/users.js
+++ front/root/_/app/js/data/users.js
@@ -3,6 +3,8 @@ $_GET.users = async function (o) {
     let data = await response ({type: 'roles'})
     
     add_vocabularies (data, {roles: 1})
+    
+    $('body').data ('data', data)
             
     return data
 
