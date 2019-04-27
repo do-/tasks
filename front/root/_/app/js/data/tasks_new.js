1f7c7a55a5876f8d095340f2481ebfcb3042965b
Dmitry Ovsyanko <do_@pochta.ru>
2019-04-27 18:28:16 +0300

    async tasks_new

--- front/root/_/app/js/data/tasks_new.js
+++ front/root/_/app/js/data/tasks_new.js
@@ -1,27 +1,25 @@
 ////////////////////////////////////////////////////////////////////////////////
 
-$_DO.update_tasks_new = function (e) {
+$_DO.update_tasks_new = async function (e) {
 
     var f = w2ui ['tasks_new_form']
 
     var v = f.values ()
     
-    if (!v.label)   die ('label', 'Конкретизируйте, пожалуйста, тему')
+    if (!v.label) die ('label', 'Конкретизируйте, пожалуйста, тему')
     
     v.img = $('input[name=img]').val ()
     v.ext = $('input[name=ext]').val ()
 
     f.lock ()
 
-    query ({action: 'create'}, {data: v}, function (data) {
+    let data = await response ({action: 'create'}, {data: v}) 
     
-        w2ui ['tasksGrid'].reload ()
+    w2ui ['tasksGrid'].reload ()
 
-        w2popup.close ()
-        
-        w2confirm ('Задача зарегистрирована. Открыть её страницу?').yes (function () {openTab ('/tasks/' + data.uuid)})
+    w2popup.close ()
     
-    })
+    w2confirm ('Задача зарегистрирована. Открыть её страницу?').yes (function () {openTab ('/tasks/' + data.uuid)})
 
 }
 
