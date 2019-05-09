98f2223926ba25bc271d218d6eec7f430be17614
Dmitry Ovsyanko <do_@pochta.ru>
2019-05-09 20:26:54 +0300

    user_password slick

--- front/root/_/app/js/data/user_password.js
+++ front/root/_/app/js/data/user_password.js
@@ -2,22 +2,26 @@
 
 $_DO.update_user_password = async function (e) {
 
-    var data = w2ui ['passwordForm'].record
+    let $this = $(e.target).closest ('.ui-dialog').find ('.ui-dialog-content')
+
+    let data = values ($this)    
     
-    if (!data.p1) return alert ('Вы не ввели пароль');
-    if (!data.p2) return alert ('Ввод пароля необходимо повторить');
+    if (!data.p1) die ('p1', 'Вы не ввели пароль');
+    if (!data.p2) die ('p2', 'Ввод пароля необходимо повторить');
     
     if (data.p1 != data.p2) return alert ('Вам не удалось ввести одно значение пароля дважды');
 
     $_REQUEST._secret = ['p1', 'p2']
     
     if ($_USER.role != 'admin') delete data.id
-    
-    await response ({type: 'users', id: $_REQUEST.id, action: 'set_password'}, {data: data})
-    
-    alert ('Пароль установлен')
 
-    w2popup.close ()
+    $this.dialog ("widget").block ()
+    
+    await response ({type: 'users', action: 'set_password'}, {data})
+    
+    $this.dialog ("close")
+
+    alert ('Пароль установлен')
 
 }
 
--- front/root/_/app/js/view/user_password.js
+++ front/root/_/app/js/view/user_password.js
@@ -1,33 +1,13 @@
 $_DRAW.user_password = async function (data) {
 
-    (await use.jq ('user_password')).w2popup ('open', {
-
-        width  : 230,
-        height : 195,
-
-        title   : 'Смена пароля',
-
-        onOpen: function (e) {
-
-            e.done (function () {               
-            
-                $('#w2ui-popup .w2ui-form').w2reform ({
-
-                    name: 'passwordForm',
-
-                    record: {},
-
-                    fields : [
-                        {name: 'p1', type: 'password'},
-                        {name: 'p2', type: 'password'},
-                    ],
-                    
-                })
-                
-            })
-            
-        }
+    let $view = fill (await use.jq ('user_password'), data).dialog ({
+        modal: true,
+        buttons: [{
+            name: 'update',
+            text: 'Установить',
+        }]
+    }).dialog ("widget")
         
-    })
-    
+    return $view
+
 }
