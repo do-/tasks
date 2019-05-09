d799127c10816d4cbb311ee0331eb34bd7b5eba2
Dmitry Ovsyanko <do_@pochta.ru>
2019-05-09 16:08:51 +0300

    login redesign; slick libs added

--- front/root/_/app/js/data/login.js
+++ front/root/_/app/js/data/login.js
@@ -2,13 +2,9 @@
 
 $_DO.execute_login = async function (e) {
         
-    let form = w2ui ['form']
-    
-    form.lock ()
-
+    $('body').block ()
+            
     let data = await response ({type: 'sessions', action: 'create'}, {data: values ($('main'))})
-
-    form.unlock ()
 
     if (!data || !data.user) return alert ('Ошибка аутентификации')
 
