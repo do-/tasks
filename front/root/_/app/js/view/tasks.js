4995b7bec40add5694744e06350303ac314f5b09
Dmitry Ovsyanko <do_@pochta.ru>
2019-05-19 21:32:56 +0300

    captions

--- front/root/_/app/js/view/tasks.js
+++ front/root/_/app/js/view/tasks.js
@@ -12,7 +12,7 @@ $_DRAW.tasks = async function (data) {
             {field: 'id_user',           name: 'На ком сейчас',    width: 40, hidden: true, formatter: (r, _, v) => data.users [v] || '[закрыто]'},
 
             {field: 'task_notes.ts',           name: 'Дата',    width: 40, formatter: (r, _, v) => v.slice (0, 19).replace ('T', ' ')},
-            {field: 'task_notes.label',        name: 'Текст',   width: 100},
+            {field: 'task_notes.label',        name: 'Последняя реплика',   width: 100},
             {field: 'task_notes.id_user_from', name: 'Автор',   width: 30, voc: data.users},
             {field: '_',           name: ' ',    width: 1, formatter: () => '&rarr;'},
             {field: 'task_notes.id_user_to',   name: 'Адресат', width: 30, voc: data.users},
