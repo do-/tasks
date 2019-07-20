c2c51f3bf92172ba8ce5a90de7f0b45afd7667e7
Dmitry Ovsyanko <do_@pochta.ru>
2019-07-20 09:20:44 +0300

    login ui FIX

--- front/root/_/app/js/data/login.js
+++ front/root/_/app/js/data/login.js
@@ -6,7 +6,7 @@ $_DO.execute_login = async function (e) {
             
     let data = await response ({type: 'sessions', action: 'create'}, {data: values ($('main'))})
 
-    if (!data || !data.user) return alert ('Ошибка аутентификации')
+    if (!data || !data.user) return $('body').unblock () && alert ('Ошибка аутентификации')
 
     $_SESSION.start (data.user, data.timeout)
                 
