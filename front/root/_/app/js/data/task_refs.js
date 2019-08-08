64c42c8c86f5751c2a331f949ff67b19c92be5d4
Dmitry Ovsyanko <do_@pochta.ru>
2019-08-08 21:27:24 +0300

    task refs show status

--- front/root/_/app/js/data/task_refs.js
+++ front/root/_/app/js/data/task_refs.js
@@ -2,7 +2,10 @@
 
 $_GET.task_refs = async function (o) {
         
-    for (i of o.refs) i.uri = '/tasks/' + i ['vw_tasks.uuid']
+    for (i of o.refs) {
+        i.uri = '/tasks/' + i ['vw_tasks.uuid']
+        i.class = 's' + i ['vw_tasks.is_open']
+    }
     
     return o
 
