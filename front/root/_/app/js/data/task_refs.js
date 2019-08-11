66d199de488d2a101197974e506b454050f3fa3c
Dmitry Ovsyanko <do_@pochta.ru>
2019-08-11 17:19:46 +0300

    task_refs status icons

--- front/root/_/app/js/data/task_refs.js
+++ front/root/_/app/js/data/task_refs.js
@@ -1,12 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 $_GET.task_refs = async function (o) {
-        
-    for (i of o.refs) {
-        i.uri = '/tasks/' + i ['vw_tasks.uuid']
-        i.class = 's' + i ['vw_tasks.is_open']
-    }
-    
+            
     return o
 
 }
--- front/root/_/app/js/view/task_refs.js
+++ front/root/_/app/js/view/task_refs.js
@@ -1,5 +1,10 @@
 $_DRAW.task_refs = async function (data) {
 
+    for (i of data.refs) {
+        i.uri = '/tasks/' + i ['vw_tasks.uuid']
+        i.class = 'status-' + i ['vw_tasks.id_status']
+    }
+
     return to_fill ('task_refs', data)
     
 }
