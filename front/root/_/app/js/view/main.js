d32e2bcc80f3dda28bfad31d9d5b31b32ef3fa1d
Dmitry Ovsyanko <do_@pochta.ru>
2019-11-24 18:01:48 +0300

    submodules updated

--- front/root/_/app/js/view/main.js
+++ front/root/_/app/js/view/main.js
@@ -1,6 +1,6 @@
 $_DRAW.main = async function (data) {
 
-    let $main = $('body').html ('<main/>')
+    let $main = $('body').html ('<main width=*/>')
 
     show_block ($_REQUEST.id ? en_unplural ($_REQUEST.type) : $_REQUEST.type)
 
