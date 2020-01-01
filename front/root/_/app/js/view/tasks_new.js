2746e34bd4d65dfdebea42118b4c7e3142a3c5d0
Dmitry Ovsyanko <do_@pochta.ru>
2020-01-01 12:02:10 +0300

    tasks_new draw_popup

--- front/root/_/app/js/view/tasks_new.js
+++ front/root/_/app/js/view/tasks_new.js
@@ -1,11 +1,7 @@
 $_DRAW.tasks_new = async function (data) {
 
-    let $view = (await to_fill ('tasks_new', data)).dialog ({width: 635,
-        modal: true,
-        buttons: [{name: 'update', text: 'Ctrl-Enter', attr: {"data-hotkey": "Ctrl-Enter"}}],
-        close:   function () {$(this).dialog ("destroy")},
-    }).dialog ("widget")
-        
+	let $view = await draw_popup ('tasks_new', data)
+
     $('#img', $view).show_block ('img')
 
     return $view
