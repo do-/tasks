7d2219e230991be3ffc6cde72b88b8c599c8053a
Dmitry Ovsyanko <do_@pochta.ru>
2020-01-07 22:09:00 +0300

    user_peers remade

--- front/root/_/app/js/data/user_peers.js
+++ front/root/_/app/js/data/user_peers.js
@@ -1,17 +1,52 @@
 ////////////////////////////////////////////////////////////////////////////////
 
+$_DO.add_user_peers = async function () {
+
+	$_DO.move_user_peers ('#grid_available', '#grid_selected')
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+$_DO.del_user_peers = async function () {
+
+	$_DO.move_user_peers ('#grid_selected', '#grid_available')
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+$_DO.move_user_peers = async function (from, to) {
+
+	let grid_from = $(from).data ('grid')
+	let grid_to = $(to).data ('grid')
+
+	let a = clone (grid_from.getData ())
+	let s = clone (grid_to.getData ())
+	let n = clone (grid_from.getSelectedRows ()).reverse ()
+
+	for (let i of n) s.push (a.splice (i, 1) [0])
+
+	grid_from.setData (a)
+	grid_from.setSelectedRows ([])
+	grid_from.render ()
+
+	grid_to.setData (s.sort ((a, b) => a.label > b.label ? 1 : a.label < b.label ? -1 : 0))
+	grid_to.setSelectedRows ([])
+	grid_to.render ()
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 $_DO.update_user_peers = async function (e) {
 
-    let $this = $(e.target).closest ('.ui-dialog').find ('.ui-dialog-content')
+    get_popup ().block ()
 
-    let data = values ($this)    
+    await response ({type: 'users', action: 'set_peers'}, {data: {
+    	ids: $('#grid_selected').data ('grid').getData ().map (i => i.uuid)
+    }})
 
-    let ids = []; for (let uuid in data) if (data [uuid] === 1) ids.push (uuid)
-
-    $this.dialog ("widget").block ()
-
-    await response ({type: 'users', action: 'set_peers'}, {data: {ids}})
-    
     $_USER.peers = (await response ({type: 'users', id: null, part: 'peers'}))
         .users.filter (i => i ['user_user.id_user'])
 
@@ -26,6 +61,12 @@ $_DO.update_user_peers = async function (e) {
 
 $_GET.user_peers = async function (o) {
 
-    return response ({type: 'users', id: null, part: 'peers'})
+    let data = await response ({type: 'users', id: null, part: 'peers'})
+
+    data.u = [[], []]
+
+    for (let user of data.users) data.u [user ['user_user.id_user'] ? 1 : 0].push (user)
+darn (data)    
+    return data
 
 }
--- front/root/_/app/js/view/user_peers.js
+++ front/root/_/app/js/view/user_peers.js
@@ -1,8 +1,56 @@
 $_DRAW.user_peers = async function (data) {
 
-    let $view = await draw_popup ('user_peers', data)
+    let $view = await draw_popup ('user_peers', data, {height: 400})
+    
+    $("#grid_available", $view).draw_table ({
 
-    for (i of data.users) if (i ['user_user.id_user']) $(`input[name=${i.uuid}]`, $view).prop ({checked: 1})
+        enableCellNavigation: false,
+
+        columns: [
+			{
+				hideInColumnTitleRow: true,
+				class: Slick.CheckboxSelectColumn,
+			},
+            {	
+            	field: "label", 
+            	name: "Доступные",
+            },
+        ],
+        
+        data: data.u [0],
+        
+        onDblClick: (e, a) => {
+        	a.grid.setSelectedRows ([a.row])
+        	$_DO.move_user_peers ('#grid_available', '#grid_selected')
+        }
+
+    })
+    
+    $("#grid_selected", $view).draw_table ({
+
+        enableCellNavigation: false,
+
+        columns: [
+			{
+				hideInColumnTitleRow: true,
+				class: Slick.CheckboxSelectColumn,
+			},
+            {	
+            	field: "label", 
+            	name: "Выбранные",
+            },
+        ],
+        
+        data: data.u [1],
+        
+        onDblClick: (e, a) => {
+        	a.grid.setSelectedRows ([a.row])
+        	$_DO.move_user_peers ('#grid_selected', '#grid_available')
+        }        
+
+    })
+
+//    for (i of data.users) if (i ['user_user.id_user']) $(`input[name=${i.uuid}]`, $view).prop ({checked: 1})
 
     return $view
 
