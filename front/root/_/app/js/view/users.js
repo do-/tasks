d49b50343c79e14e5e63846bea1aa28d66332df7
Dmitry Ovsyanko <do_@pochta.ru>
2022-01-07 16:33:46 +0300

    users dx grid open on enter key

--- front/root/_/app/js/view/users.js
+++ front/root/_/app/js/view/users.js
@@ -5,23 +5,30 @@ $_DRAW.users = async function (data) {
     let $result = $('main').html (await use.html ('users'))
 
 	$('#grid_users').dxDataGrid({
+
 		dataSource: data.src,
+
 		showBorders: true,
 		showRowLines: true,
+		hoverStateEnabled: true,
 		focusedRowEnabled: true,
 		remoteOperations: true, 
 		allowColumnResizing: true,
+
 		pager: {
 			visible: true,
 			showInfo: true,
 			showNavigationButtons: false,
 		},
+
 		scrolling: {
 			mode: 'virtual',
 		},
+
 		searchPanel: {
 			visible: true,
 		},
+
 		columns: [
 			{
 				dataField: 'label',
@@ -38,10 +45,28 @@ $_DRAW.users = async function (data) {
 				dataField: 'mail',
 				caption: 'E-mail',
 				dataType: 'string',
-			},    
-			//            {field: "id_role", name: "Роль", width: 50, voc: data.roles},    
+			},
+			{
+				dataField: 'id_role',
+				caption: 'Роль',
+				lookup: {
+					dataSource: data.roles.items,
+					valueExpr: 'id',
+					displayExpr: 'label',
+ 				}
+			},
 		],
+
 		onRowDblClick: e => open_tab ('/users/' + e.data.uuid),    
+		
+		onKeyDown: e => {
+		
+			if (e.event.which !== 13) return
+			
+			open_tab ('/users/' + e.component.option ('focusedRowKey'))
+		
+		}
+
 	}).dxDataGrid('instance');
 
     return $result
