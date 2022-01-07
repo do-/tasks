aa8b41b44c060ed0e68d61161cbd8fcee36e2363
Dmitry Ovsyanko <do_@pochta.ru>
2022-01-07 13:28:03 +0300

    users: 1st dxgrid somehow working

--- front/root/_/app/js/view/users.js
+++ front/root/_/app/js/view/users.js
@@ -3,27 +3,72 @@ $_DRAW.users = async function (data) {
     $('title').text ('Пользователи системы')
     
     let $result = $('main').html (await use.html ('users'))
-
-    let grid = $("#grid_users").draw_table ({
-
-        columns: [
-            {field: "label", name: "ФИО", width: 200, sortable: true},
-            {field: "login", name: "Login", width: 50, sortable: true},
-            {field: "mail", name: "E-mail", width: 100, sortable: true},
-            {field: "id_role", name: "Роль", width: 50, voc: data.roles},
-        ],
-        
-        searchInputs: 
-            $(".toolbar :input").toArray ()
-        ,
-        
-        src: data.src,
-        
-        onRecordDblClick: (r) => open_tab ('/users/' + r.uuid),
-
-    })
     
-    $(".toolbar input:first").focus ()
+const store = new DevExpress.data.CustomStore ({
+
+    key: 'uuid',
+    load: async loadOptions => {
+darn (loadOptions)
+		const {skip, take, sort} = loadOptions
+
+		let o = {
+			searchLogic: "AND",
+			limit:take,
+			offset:skip,
+			search: [],
+		}
+
+		if (sort) o.sort = sort.map (i => ({
+			field: i.selector,
+			direction: i.desc ? 'desc' : 'asc',
+		}))
+
+		const {users, cnt} = await response ({type: 'users', id: null}, o)
+
+		return {
+			data: users,
+			totalCount: cnt,
+		}
+
+    },
+
+})
+
+  $('#grid_users').dxDataGrid({
+    dataSource: store,
+    showBorders: true,
+    remoteOperations: true,
+    paging: {
+      pageSize: 12,
+    },
+    pager: {
+      showPageSizeSelector: true,
+      allowedPageSizes: [8, 12, 20],
+    },
+    columns: [
+    {
+      dataField: 'label',
+      caption: 'ФИО',
+      dataType: 'string',
+    },
+    {
+      dataField: 'login',
+       caption: 'Login',
+     dataType: 'string',
+    },
+    {
+      dataField: 'mail',
+       caption: 'E-mail',
+      dataType: 'string',
+    },
+    
+//            {field: "id_role", name: "Роль", width: 50, voc: data.roles},
+    
+    ],
+    
+    onRowDblClick: e => open_tab ('/users/' + e.data.uuid),
+    
+  }).dxDataGrid('instance');
 
     return $result
 
