494970cf9da19f07ecc08955b0eb2c2d7099adda
Dmitry Ovsyanko <do_@pochta.ru>
2022-01-07 10:29:36 +0300

    vw_user_options model FIX: `is_on` was selected, not declared

--- back/lib/Model/vw_user_options.js
+++ back/lib/Model/vw_user_options.js
@@ -5,7 +5,8 @@ module.exports = {
     columns : {
         id_user            : '(users)            // Пользователь', 
         id_voc_user_option : '(voc_user_options) // Опция', 
-    },
+        is_on              : 'int=0              // Статус (0 — нет, 1 — есть)', 
+	},
 
     pk : ['id_user', 'id_voc_user_option'],
     
