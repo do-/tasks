eb9a2317a5f08cd552c7e54c6d67239de7c867c4
Dmitry Ovsyanko <do_@pochta.ru>
2023-10-18 20:22:50 +0300

    doix updateModel

--- back_2/lib/Content/app.js
+++ back_2/lib/Content/app.js
@@ -6,17 +6,7 @@ do_init_app:
 
     async function () {
 
-        await this.db.pool.toSet (this, 'db')
-    
-    	const {db} = this
-
-        const plan = db.createMigrationPlan ()
-
-        await plan.loadStructure ()
-
-        plan.inspectStructure ()
-
-    	for (const [sql, params] of plan.genDDL ()) await db.do (sql, params)
+        await this.db.updateModel ()
 
     },
     
--- back_2/lib/DB.js
+++ back_2/lib/DB.js
@@ -1,5 +1,5 @@
 const {DbPoolPg} = require ('doix-db-postgresql')
-//const w2ui       = require ('doix-w2ui')
+const w2ui       = require ('doix-w2ui')
 const Model      = require ('./Model.js')
 
 module.exports = class extends DbPoolPg {
@@ -8,9 +8,27 @@ module.exports = class extends DbPoolPg {
 
 		super ({db, logger})
 
-//		w2ui.plugInto (this)
+		w2ui.plugInto (this)
 
 		new Model (this)
+
+		this.shared.add ('updateModel')
+
+	}
+
+	async updateModel () {
+
+		const {job, pool} = this
+
+		await pool.toSet (job, 'db')
+
+        const {db} = job, plan = db.createMigrationPlan ()
+
+        await plan.loadStructure ()
+
+        plan.inspectStructure ()
+
+    	for (const [sql, params] of plan.genDDL ()) await db.do (sql, params)
 
 	}
 
