72b278cf2d587a0da36b40dd06404341bc366f5d
Dmitry Ovsyanko <do_@pochta.ru>
2024-02-04 18:19:46 +0300

    doix select_tasks

---
+++ back_2/lib/Content/tasks.js
@@ -0,0 +1,44 @@
+module.exports = {
+
+////////////////////////////////////////////////////////////////////////////////
+
+get_vocs_of_tasks:
+
+    async function () {
+
+		const {db} = this, {model}= db, SELECT_FROM = "SELECT JSONB_AGG (JSONB_BUILD_OBJECT ('id', uuid, 'label', label) ORDER BY label) FROM"
+
+        const data = await db.getScalar (/*sql*/`
+            SELECT JSONB_BUILD_OBJECT (
+                'users',        (${SELECT_FROM} users WHERE uuid IN (SELECT DISTINCT id_user FROM task_users)),
+                'voc_projects', (${SELECT_FROM} voc_projects)
+            )
+        `)
+
+        for (const k of ['voc_task_status']) data [k] = model.find (k).data
+
+        return data
+
+    },
+
+////////////////////////////////////////////////////////////////////////////////
+
+select_tasks: 
+
+    function () {
+
+    	const {db} = this
+
+        this.rq.loadOptions.sort = [{selector: 'ts'}]
+
+		const q = db.dxQuery (
+			[
+				['vw_tasks', {as: 'tasks'}],
+			]
+        )
+
+		return db.getArray (q)
+
+    },
+       
+}
