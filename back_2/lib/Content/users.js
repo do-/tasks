5832dd46b2e77bbc7f52f466d29ee410cc0092a0
Dmitry Ovsyanko <do_@pochta.ru>
2024-03-24 11:05:01 +0300

    do_set_password_users: throw error, not string

--- back_2/lib/Content/users.js
+++ back_2/lib/Content/users.js
@@ -92,8 +92,8 @@ do_set_password_users:
 
     	const {db, rq, user, pwd, http: {request: {headers}}} = this, p1 = headers ['x-request-param-p1'], p2 = headers ['x-request-param-p1']
 
-		if (p1 == null) throw '#p1#: Получено пустое значение пароля'
-        if (p1 != p2)   throw '#p2#: Повторное значение пароля не сходится'
+		if (p1 == null) throw Error ('#p1#: Получено пустое значение пароля')
+        if (p1 != p2)   throw Error ('#p2#: Повторное значение пароля не сходится')
 
         const salt     = pwd.sprinkle (32)
         const password = pwd.cook (p1, salt)
